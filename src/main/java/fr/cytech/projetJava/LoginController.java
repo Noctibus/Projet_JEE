8fc55c7c89e01cbd048375184b30505a9ba2409a
tcoyla <coyla.timotei@gmail.com>
2022-11-23 12:58:22 +0100

    fix problem

--- src/main/java/fr/cytech/projetJava/LoginController.java
+++ src/main/java/fr/cytech/projetJava/LoginController.java
@@ -33,8 +33,8 @@ public class LoginController {
 	}
 
 	@GetMapping("/")
-	public String index(Model model) {
-		return "index";
+	public String logs(Model model) {
+		return login(model);
 	}
 
 	@GetMapping("/login")

ba626d16866bdde3b78ed5f749852e6d0d3da0b8
Noctibus <61824649+Noctibus@users.noreply.github.com>
2022-11-23 09:49:42 +0100

    Add files via upload

---
+++ src/test/java/fr/cytech/ProjetJEE/ProjetJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cytech.ProjetJEE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

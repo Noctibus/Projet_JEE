9a7cafae046ed1747a216058ed94d515569bc8c4
tcoyla <coyla.timotei@gmail.com>
2022-11-24 08:33:49 +0100

    create rate files

---
+++ src/main/java/fr/cytech/projetJava/rate/characterRate.java
@@ -0,0 +1,10 @@
+package fr.cytech.projetJava.rate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class characterRate extends Rate {
+    
+}

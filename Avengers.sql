47e2f5efd7fdfdfbfd8ffcf2af44a2eb32f6fbd2
tcoyla <coyla.timotei@gmail.com>
2022-11-24 11:03:48 +0100

    update Avengers.sql
    change title in english and add rates databases

--- Avengers.sql
+++ Avengers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb:3306
--- Generation Time: Nov 21, 2022 at 09:12 PM
+-- Generation Time: Nov 24, 2022 at 10:02 AM
 -- Server version: 10.6.10-MariaDB-1:10.6.10+maria~ubu2004
 -- PHP Version: 8.0.24
 
@@ -24,57 +24,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Films`
+-- Table structure for table `CharacterRates`
 --
 
-CREATE TABLE `Films` (
-  `Titre` text DEFAULT NULL,
-  `Réalisateur` text DEFAULT NULL,
-  `Date` date DEFAULT NULL,
-  `Note` float(5,0) DEFAULT NULL,
-  `Synobsis` text DEFAULT NULL,
-  `Id` int(11) NOT NULL
+CREATE TABLE `CharacterRates` (
+  `UserID` int(11) NOT NULL,
+  `Value` int(11) NOT NULL,
+  `CharacterID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `Films`
---
-
-INSERT INTO `Films` (`Titre`, `Réalisateur`, `Date`, `Note`, `Synobsis`, `Id`) VALUES
-('Avengers', 'Joss Whedon', '2012-04-20', 3, 'Lorsque Nick Fury, le directeur du S.H.I.E.L.D., l\'organisation qui préserve la paix au plan mondial, cherche à former une équipe de choc pour empêcher la destruction du monde, Iron Man, Hulk, Thor, Captain America, Hawkeye et Black Widow répondent présents.\r\nLes Avengers ont beau constituer la plus fantastique des équipes, il leur reste encore à apprendre à travailler ensemble, et non les uns contre les autres, d\'autant que le redoutable Loki a réussi à accéder au Cube Cosmique et à son pouvoir illimité... ', 1),
-('Avengers : L’Ère d’Ultron ', 'Joss Whedon', '2015-04-22', 2, 'Alors que Tony Stark tente de relancer un programme de maintien de la paix jusque-là suspendu, les choses tournent mal et les super-héros Iron Man, Captain America, Thor, Hulk, Black Widow et Hawkeye vont devoir à nouveau unir leurs forces pour combattre le plus puissant de leurs adversaires : le terrible Ultron, un être technologique terrifiant qui s’est juré d’éradiquer l’espèce humaine.\r\nAfin d’empêcher celui-ci d’accomplir ses sombres desseins, des alliances inattendues se scellent, les entraînant dans une incroyable aventure et une haletante course contre le temps… ', 2),
-('Avengers : Infinity War', 'Joe Russo, Anthony Russo', '2018-04-25', 5, 'Les Avengers et leurs alliés devront être prêts à tout sacrifier pour neutraliser le redoutable Thanos avant que son attaque éclair ne conduise à la destruction complète de l’univers. ', 3),
-('Avengers: Endgame', 'Joe Russo, Anthony Russo', '2019-04-24', 4, 'Thanos ayant anéanti la moitié de l’univers, les Avengers restants resserrent les rangs dans ce vingt-deuxième film des Studios Marvel, grande conclusion d’un des chapitres de l’Univers Cinématographique Marvel.', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Personnages`
+-- Table structure for table `Characters`
 --
 
-CREATE TABLE `Personnages` (
+CREATE TABLE `Characters` (
   `Id` int(11) NOT NULL,
-  `Identité` text DEFAULT NULL,
-  `Nom de héro` text DEFAULT NULL,
-  `Acteur` text NOT NULL,
-  `Origines` text NOT NULL,
+  `Identity` text DEFAULT NULL,
+  `Hero's name` text DEFAULT NULL,
+  `Actor` text NOT NULL,
+  `Origins` text NOT NULL,
   `Avengers1` tinyint(4) NOT NULL DEFAULT 1,
   `Avengers2` tinyint(4) NOT NULL DEFAULT 1,
   `Avengers3` tinyint(4) NOT NULL DEFAULT 1,
   `Avengers4` tinyint(4) NOT NULL DEFAULT 1,
-  `Note` float DEFAULT NULL,
-  `Etat` tinyint(1) NOT NULL DEFAULT 1
+  `Rate` float DEFAULT NULL,
+  `Status` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Personnages`
+-- Dumping data for table `Characters`
 --
 
-INSERT INTO `Personnages` (`Id`, `Identité`, `Nom de héro`, `Acteur`, `Origines`, `Avengers1`, `Avengers2`, `Avengers3`, `Avengers4`, `Note`, `Etat`) VALUES
-(1, 'Anthony Stark', 'Iron Man', 'Robert Downey Jr.', 'Anthony Edward « Tony » Stark est un industriel américain, née à Long Island dans l\'État de New York fils de Howard Stark un scientifique avec un goût prononcé pour l\'alcool à la tête de Stark Industries et de Maria Collins-Carbonell Stark. Avec l\'entreprise de son père, il fabrique notamment des armes pour le gouvernement américain. Cela l\'amène à se rendre au Viet Nâm alors en guerre contre les États-Unis pour démontrer la puissance des «transistors» qui permettent de décupler la puissance de n\'importe quelle arme. Mais il saute malencontreusement sur une mine qui projette des éclats près de son cœur.', 1, 1, 1, 1, 1, 0),
+INSERT INTO `Characters` (`Id`, `Identity`, `Hero's name`, `Actor`, `Origins`, `Avengers1`, `Avengers2`, `Avengers3`, `Avengers4`, `Rate`, `Status`) VALUES
+(1, 'Anthony Stark', 'Iron Man', 'Robert Downey Jr.', 'Anthony Edward « Tony » Stark est un industriel américain, née à Long Island dans l\'État de New York fils de Howard Stark un scientifique avec un goût prononcé pour l\'alcool à la tête de Stark Industries et de Maria Collins-Carbonell Stark. Avec l\'entreprise de son père, il fabrique notamment des armes pour le gouvernement américain. Cela l\'amène à se rendre au Viet Nâm alors en guerre contre les États-Unis pour démontrer la puissance des «transistors» qui permettent de décupler la puissance de n\'importe quelle arme. Mais il saute malencontreusement sur une mine qui projette des éclats près de son cœur.', 1, 1, 1, 1, 5, 0),
 (2, 'Thor', NULL, 'Chris Hemsworth\r\n', 'Dans la mythologie scandinave, Thor (en vieux norrois : Þórr, littéralement « foudre ») est le dieu du tonnerre, de la guerre et de la forge. C\'est un dieu guerrier très puissant qui représente le combat, la force et le courage. Il est grand et fort. C\'est un gros mangeur et buveur.', 1, 1, 1, 1, 5, 1),
 (3, 'Bruce Banner', 'Hulk', 'Mark Ruffalo, Eric Bana', 'Quand Bruce Banner reçut des rayons gamma de plein fouet, il fut condamné à se transformer en colosse vert, dès qu\'il se met en colère.', 1, 1, 1, 1, 4, 1),
-(4, 'Steven Rogers', 'Captain America', 'Chris Evans', 'Après avoir avalé le sérum du super-soldat durant la Seconde Guerre mondiale, Steven Rogers est devenu un super-héros sous le nom de Captain America.', 0, 0, 1, 1, 5, 0),
+(4, 'Steven Rogers', 'Captain America', 'Chris Evans', 'Après avoir avalé le sérum du super-soldat durant la Seconde Guerre mondiale, Steven Rogers est devenu un super-héros sous le nom de Captain America.', 0, 0, 1, 1, 1, 0),
 (5, 'Natasha Romanoff', 'Black Widow ', 'Scarlett Johansson', 'Abandonnée à sa naissance, Natasha a été formée dans la chambre Rouge, un centre d’entraînement pour espions top secret. C’est là qu’elle a été formée aux arts martiaux et aux techniques d’espionnage. Elle est en sortie en tant que la Veuve Noire, une espionne de haut niveau. La Veuve Noire est entraînée à la perfection. Cela lui donne un niveau extrème de force, d’agilité et d’endurance.', 1, 1, 1, 1, 4, 0),
 (6, 'Clinton Barton', 'Hawkeye', 'Jeremy Renner', 'Enfant, Clint Francis Barton s\'échappe d\'un orphelinat pour rejoindre un cirque. Entrainé par Trickshot et Swordsman, il rejette ce dernier quand il découvre qu\'il est un criminel.', 1, 1, 1, 1, 4, 1),
 (7, 'Peter Parker', 'Spider-Man', 'Tom Holland', 'Orphelin depuis l’âge de 6 ans, Peter Parker a été confié à son oncle et à sa tante, Benjamin et May Parker. Fragile émotionnellement et physiquement, Peter développe très tôt un intérêt pour les sciences et c’est au cours d’une expérience à laquelle il assiste qu’il est mordu par une araignée radioactive. Cette morsure lui octroie des super-pouvoirs : il peut s’accrocher à n’importe quelle surface et escalader un bâtiment ou toute autre construction à la manière d’une araignée, son agilité et ses réflexes étant exacerbés. L’ensemble de ses capacités (force, endurance, résistance, sens et capacités de régénération) sont de loin supérieures à celles d’un être humain ordinaire et il possède un sixième sens : un « sens d’araignée » qui l’avertit de tout danger imminent. Last but not least, Spider-Man peut également projeter de ses mains une toile extrêment solide et collante, qui lui permet d’immobiliser ses ennemis ou de se déplacer plus rapidement.', 0, 0, 1, 1, 4, 1),
@@ -98,95 +85,83 @@ INSERT INTO `Personnages` (`Id`, `Identité`, `Nom de héro`, `Acteur`, `Origine
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Utilisateurs`
+-- Table structure for table `hibernate_sequence`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` text DEFAULT NULL,
-  `password` text DEFAULT NULL,
-  `NoteAv1` int(5) DEFAULT NULL,
-  `NoteAv2` int(5) DEFAULT NULL,
-  `NoteAv3` int(5) DEFAULT NULL,
-  `NoteAv4` int(5) DEFAULT NULL,
-  `NoteAv5` int(5) DEFAULT NULL,
-  `NoteP1` int(5) DEFAULT NULL,
-  `NoteP2` int(5) DEFAULT NULL,
-  `NoteP3` int(5) DEFAULT NULL,
-  `NoteP4` int(5) DEFAULT NULL,
-  `NoteP5` int(5) DEFAULT NULL,
-  `NoteP6` int(5) DEFAULT NULL,
-  `NoteP7` int(5) DEFAULT NULL,
-  `NoteP8` int(5) DEFAULT NULL,
-  `NoteP9` int(5) DEFAULT NULL,
-  `NoteP10` int(5) DEFAULT NULL,
-  `NoteP11` int(5) DEFAULT NULL,
-  `NoteP12` int(5) DEFAULT NULL,
-  `NoteP13` int(5) DEFAULT NULL,
-  `NoteP14` int(5) DEFAULT NULL,
-  `NoteP15` int(5) DEFAULT NULL,
-  `NoteP16` int(5) DEFAULT NULL,
-  `NoteP17` int(5) DEFAULT NULL,
-  `NoteP18` int(5) DEFAULT NULL,
-  `NoteP19` int(5) DEFAULT NULL,
-  `NoteP20` int(5) DEFAULT NULL,
-  `NoteP21` int(5) DEFAULT NULL,
-  `NoteP22` int(5) DEFAULT NULL,
-  `NoteP23` int(5) DEFAULT NULL
+CREATE TABLE `hibernate_sequence` (
+  `next_not_cached_value` bigint(21) NOT NULL,
+  `minimum_value` bigint(21) NOT NULL,
+  `maximum_value` bigint(21) NOT NULL,
+  `start_value` bigint(21) NOT NULL COMMENT 'start value when sequences is created or value if RESTART is used',
+  `increment` bigint(21) NOT NULL COMMENT 'increment value',
+  `cache_size` bigint(21) UNSIGNED NOT NULL,
+  `cycle_option` tinyint(1) UNSIGNED NOT NULL COMMENT '0 if no cycles are allowed, 1 if the sequence should begin a new cycle when maximum_value is passed',
+  `cycle_count` bigint(21) NOT NULL COMMENT 'How many cycles have been done'
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_not_cached_value`, `minimum_value`, `maximum_value`, `start_value`, `increment`, `cache_size`, `cycle_option`, `cycle_count`) VALUES
+(4001, 1, 9223372036854775806, 1, 1, 1000, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `MovieRates`
+--
+
+CREATE TABLE `MovieRates` (
+  `UserID` int(11) NOT NULL,
+  `Value` int(11) NOT NULL,
+  `MovieID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Movies`
+--
+
+CREATE TABLE `Movies` (
+  `Title` text DEFAULT NULL,
+  `Realisator` text DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Rate` float(5,0) DEFAULT NULL,
+  `Synopsis` text DEFAULT NULL,
+  `Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Utilisateurs`
+-- Dumping data for table `Movies`
 --
 
-INSERT INTO `Utilisateurs` (`Id`, `Identifiant`, `Mdp`, `NoteAv1`, `NoteAv2`, `NoteAv3`, `NoteAv4`, `NoteAv5`, `NoteP1`, `NoteP2`, `NoteP3`, `NoteP4`, `NoteP5`, `NoteP6`, `NoteP7`, `NoteP8`, `NoteP9`, `NoteP10`, `NoteP11`, `NoteP12`, `NoteP13`, `NoteP14`, `NoteP15`, `NoteP16`, `NoteP17`, `NoteP18`, `NoteP19`, `NoteP20`, `NoteP21`, `NoteP22`, `NoteP23`) VALUES
-(1, 'Mel', '1234', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, 'Stan', '0000', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `Movies` (`Title`, `Realisator`, `Date`, `Rate`, `Synopsis`, `Id`) VALUES
+('Avengers', 'Joss Whedon', '2012-04-20', 3, 'Lorsque Nick Fury, le directeur du S.H.I.E.L.D., l\'organisation qui préserve la paix au plan mondial, cherche à former une équipe de choc pour empêcher la destruction du monde, Iron Man, Hulk, Thor, Captain America, Hawkeye et Black Widow répondent présents.\r\nLes Avengers ont beau constituer la plus fantastique des équipes, il leur reste encore à apprendre à travailler ensemble, et non les uns contre les autres, d\'autant que le redoutable Loki a réussi à accéder au Cube Cosmique et à son pouvoir illimité... ', 1),
+('Avengers : L’Ère d’Ultron ', 'Joss Whedon', '2015-04-22', 2, 'Alors que Tony Stark tente de relancer un programme de maintien de la paix jusque-là suspendu, les choses tournent mal et les super-héros Iron Man, Captain America, Thor, Hulk, Black Widow et Hawkeye vont devoir à nouveau unir leurs forces pour combattre le plus puissant de leurs adversaires : le terrible Ultron, un être technologique terrifiant qui s’est juré d’éradiquer l’espèce humaine.\r\nAfin d’empêcher celui-ci d’accomplir ses sombres desseins, des alliances inattendues se scellent, les entraînant dans une incroyable aventure et une haletante course contre le temps… ', 2),
+('Avengers : Infinity War', 'Joe Russo, Anthony Russo', '2018-04-25', 5, 'Les Avengers et leurs alliés devront être prêts à tout sacrifier pour neutraliser le redoutable Thanos avant que son attaque éclair ne conduise à la destruction complète de l’univers. ', 3),
+('Avengers: Endgame', 'Joe Russo, Anthony Russo', '2019-04-24', 4, 'Thanos ayant anéanti la moitié de l’univers, les Avengers restants resserrent les rangs dans ce vingt-deuxième film des Studios Marvel, grande conclusion d’un des chapitres de l’Univers Cinématographique Marvel.', 4);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `User`
 --
 
---
--- Indexes for table `Films`
---
-ALTER TABLE `Films`
-  ADD PRIMARY KEY (`Id`);
+CREATE TABLE `User` (
+  `ID` int(11) NOT NULL,
+  `Username` text DEFAULT NULL,
+  `Password` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for table `Personnages`
---
-ALTER TABLE `Personnages`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indexes for table `Utilisateurs`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`) USING HASH;
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `User`
 --
 
---
--- AUTO_INCREMENT for table `Films`
---
-ALTER TABLE `Films`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `Personnages`
---
-ALTER TABLE `Personnages`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
--- AUTO_INCREMENT for table `Utilisateurs`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+INSERT INTO `User` (`ID`, `Username`, `Password`) VALUES
+(2001, 'Bob', '0000'),
+(3001, 'Bob', '0000');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

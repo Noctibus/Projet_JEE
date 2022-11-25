97494ae7e2f79aa367bafb37b7dca43543799d2b
Noctibus <stanislas.debet@gmail.com>
2022-11-25 13:50:35 +0100

    Register Saving plan
    ajout des fichiers register

--- src/main/java/fr/cytech/projetJava/login/LoginController.java
+++ src/main/java/fr/cytech/projetJava/login/LoginController.java
@@ -23,7 +23,11 @@ public class LoginController {
 		System.out.println("User = " + user);
 		System.out.println("Usr  = " + usr);
 		if (!Objects.isNull(usr)) {
+<<<<<<< HEAD
 			if (usr.getPassword().equals(user.getPassword())) {
+=======
+			if (usr.getPassword().equals(password)) {
+>>>>>>> parent of 117ae80 (Register)
 				page = "logged";
 			}
 		}
@@ -39,6 +43,7 @@ public class LoginController {
 		return "login"; // page (html, xml ou autre) à afficher
 	}
 	
+<<<<<<< HEAD
 	@RequestMapping(value="/logged", method=RequestMethod.POST)
 	public String logging(@RequestParam("username") String username, @RequestParam("password") String password) {
 		System.out.println("+++++++++++++++++\nUsername = " + username);
@@ -46,4 +51,34 @@ public class LoginController {
 		user.setPassword(password);
 		return check(user);
 	}
+=======
+	@PostMapping("/logged")
+	public String logged() {
+		// System.out.println("+++++++++++++++++\nUsername = " + username);
+		return "/logged";
+	}
+	
+	@GetMapping("/register")
+	public String register(Model model) {
+		return "register";
+	}
+
+	@PostMapping("/registerNewUser")
+	public String registerNewUser(Model model,@RequestParam("username") String username,@RequestParam("password1") String password1,@RequestParam("password2") String password2) {
+		boolean res = true;
+		String page = "register";
+		User usr = userRepository.findByUsername(user.getUsername());
+		if (!Objects.isNull(usr) || !password1.equals(password2)) {
+			res = false;
+		}
+		if (res) {
+			user.setUsername(username);
+			user.setPassword(password1);
+			userRepository.save(user);
+			page = "logged";
+		}
+		return page;
+	}
+	
+>>>>>>> parent of 117ae80 (Register)
 }

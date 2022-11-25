3c525e4b4ec3340341d9f75c1e59c1f75f90f374
Raphystolage <raphael.morales31@gmail.com>
2022-11-25 16:58:00 +0100

    upd .java

--- src/main/java/fr/cytech/projetJava/FrontController.java
+++ src/main/java/fr/cytech/projetJava/FrontController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FrontController{
+public class FrontController {
 
     @GetMapping(value="/")
 	public String index() {

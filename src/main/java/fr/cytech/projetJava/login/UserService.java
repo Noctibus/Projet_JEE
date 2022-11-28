0293e423fd3d9209d1252d9f47a68cdacc03986a
tcoyla <coyla.timotei@gmail.com>
2022-11-28 11:00:02 +0100

    update userService

--- src/main/java/fr/cytech/projetJava/login/UserService.java
+++ src/main/java/fr/cytech/projetJava/login/UserService.java
@@ -11,11 +11,18 @@ public class UserService {
     @Autowired
  	private UserRepository userRepository;
 
-    public User getUserByUsername(String username) {
+    public User getByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
-    public void saveUser(User user) {
+    public User getByID(int id) {
+        return userRepository.findById(id);
+    }
+    
+    public void createUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
         userRepository.save(user);
     }
 

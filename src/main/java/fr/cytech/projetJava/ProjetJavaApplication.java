1619238a54a17248f2d5032eba9cc4613e74d1b6
tcoyla <coyla.timotei@gmail.com>
2022-12-06 15:19:45 +0100

    clear javaApplication

--- src/main/java/fr/cytech/projetJava/ProjetJavaApplication.java
+++ src/main/java/fr/cytech/projetJava/ProjetJavaApplication.java
@@ -1,25 +1,8 @@
 package fr.cytech.projetJava;
 
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-<<<<<<< HEAD
-=======
-import fr.cytech.projetJava.comments.CharacterComment;
-import fr.cytech.projetJava.comments.CharacterCommentService;
-import fr.cytech.projetJava.comments.MovieComment;
-import fr.cytech.projetJava.comments.MovieCommentService;
-import fr.cytech.projetJava.login.User;
->>>>>>> 75a1ddbd6cd24e59c28756f1c12125b1b05c28c1
-import fr.cytech.projetJava.login.UserRepository;
-import fr.cytech.projetJava.character.Character;
-import fr.cytech.projetJava.character.CharacterRepository;
 
 @SpringBootApplication
 public class ProjetJavaApplication implements CommandLineRunner {
@@ -29,21 +12,9 @@ public class ProjetJavaApplication implements CommandLineRunner {
 		SpringApplication.run(ProjetJavaApplication.class, args);
 	}
 
-<<<<<<< HEAD
-	@Autowired
- 	UserRepository userRepository;
-	 
-	@Bean
-    public CommandLineRunner CommandLineRunnerBean() {
-        return (args) -> {
-            System.out.println("Démarrage en cours...");
-        };
-=======
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Démarrage en cours...");
->>>>>>> 75a1ddbd6cd24e59c28756f1c12125b1b05c28c1
     }
-
-
+    
 }

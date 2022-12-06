00f7dd0736f32153635e37de8c6bd7b6a8d17271
tcoyla <coyla.timotei@gmail.com>
2022-12-06 15:13:00 +0100

    clear javaApplication

--- src/main/java/fr/cytech/projetJava/ProjetJavaApplication.java
+++ src/main/java/fr/cytech/projetJava/ProjetJavaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import fr.cytech.projetJava.login.User;
 import fr.cytech.projetJava.login.UserRepository;
 
 @SpringBootApplication
@@ -23,17 +22,6 @@ public class ProjetJavaApplication {
     public CommandLineRunner CommandLineRunnerBean() {
         return (args) -> {
             System.out.println("Démarrage en cours...");
-
-        	// creer le nouvel utilisateur Bob
-
-            //User user = new User();
-            // user.setUsername("Bob");
-            // user.setPassword("0000");
-            // userRepository.save(user);
-            
-            for(User tmp : userRepository.findAll()) {
-            	System.out.println(tmp);
-            }
         };
     }
 }
